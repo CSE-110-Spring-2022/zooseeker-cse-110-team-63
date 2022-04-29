49bc7ad6f7faeb0013f4af914c2a0e4e7bb5ee59
eylau-ucsd <eylau@ucsd.edu>
2022-04-28 21:13:57 -0700

    initialize empty activity

---
+++ app/src/main/java/com/team63/zooseeker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team63.zooseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
