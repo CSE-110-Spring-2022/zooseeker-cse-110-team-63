ba92b5ea9bc89bdbf82ace35b751a5e4f7606a1c
Ishaan Harry Kavoori <ishaanharry@live.com>
2022-04-29 12:30:10 -0700

    made interface for display directions screen

---
+++ app/src/main/java/com/team63/zooseeker/DirectionActivity.java
@@ -0,0 +1,14 @@
+package com.team63.zooseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DirectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction);
+    }
+}
