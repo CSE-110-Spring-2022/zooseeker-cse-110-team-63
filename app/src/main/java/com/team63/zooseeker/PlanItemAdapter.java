9bc32a59db265ee2f858fb1c74b75b10f928845a
eylau-ucsd <eylau@ucsd.edu>
2022-04-30 17:51:44 -0700

    create UI for "view plan" screen

---
+++ app/src/main/java/com/team63/zooseeker/PlanItemAdapter.java
@@ -0,0 +1,59 @@
+package com.team63.zooseeker;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PlanItemAdapter extends RecyclerView.Adapter<PlanItemAdapter.ViewHolder> {
+    private List<Direction> planItems = Collections.emptyList();
+
+    public void setPlanItems(List<Direction> newPlanItems) {
+        this.planItems.clear();
+        this.planItems = newPlanItems;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.plan_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setPlanItem(planItems.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return planItems.size();
+    }
+
+    @Override
+    public long getItemId(int position) { return planItems.get(position).id; }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+        private Direction planItem;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textView = itemView.findViewById(R.id.plan_item_text);
+        }
+        public Direction getPlanItem() { return planItem; }
+
+        public void setPlanItem(Direction planItem) {
+            this.planItem = planItem;
+            this.textView.setText(planItem.name);
+        }
+    }
+}
---
+++ app/src/main/java/com/team63/zooseeker/ViewPlanActivity.java
@@ -0,0 +1,61 @@
+package com.team63.zooseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class ViewPlanActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_plan);
+
+        PlanItemAdapter adapter = new PlanItemAdapter();
+        adapter.setHasStableIds(true);
+
+        recyclerView = findViewById(R.id.plan_items);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration divider = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+        recyclerView.addItemDecoration(divider);
+        recyclerView.setAdapter(adapter);
+
+        // demo code - delete later
+        Graph<String, IdentifiedWeightedEdge> G = ZooData.loadZooGraphJSON(this, "sample_zoo_graph.json");
+        List<String> targets = Arrays.asList("elephant_odyssey", "arctic_foxes", "gators", "gorillas");
+        RouteGenerator demoRouteGen = new NNRouteGenerator(G);
+        ArrayList<Direction> demoPlan = new ArrayList<>();
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
+        Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON(this, "sample_edge_info.json");
+        for (GraphPath<String, IdentifiedWeightedEdge> path : demoRouteGen.getRoute("entrance_exit_gate", targets)) {
+            demoPlan.add(new Direction(path, vInfo, eInfo));
+        }
+        List<Direction> plan = demoPlan;
+        // end of demo code
+
+        List<Direction> planWithoutExit = new ArrayList<>(plan);
+        planWithoutExit.remove(planWithoutExit.size() - 1);
+        adapter.setPlanItems(planWithoutExit);
+        TextView planCount = findViewById(R.id.plan_count);
+        planCount.setText(String.format(Locale.US, "Plan (%d)", adapter.getItemCount()));
+
+
+    }
+}
