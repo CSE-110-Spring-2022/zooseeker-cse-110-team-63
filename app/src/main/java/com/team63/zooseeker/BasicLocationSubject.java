40ffe242bd941f4262ee809d653b57b26b0f3aa8
eylau-ucsd <eylau@ucsd.edu>
2022-05-30 20:54:57 -0700

    fix weird merge conflict

--- app/src/main/java/com/team63/zooseeker/BasicLocationSubject.java
+++ app/src/main/java/com/team63/zooseeker/BasicLocationSubject.java
@@ -18,28 +18,6 @@ public class BasicLocationSubject implements LocationSubject {
 
     public BasicLocationSubject(Map<String, ZooData.VertexInfo> zooMap) {
         this.zooMap = zooMap;
-<<<<<<<< HEAD:app/src/main/java/com/team63/zooseeker/BasicLocationSubject.java
-========
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                Log.d("ZooSeeker", "onLocationChanged called");
-                if (latitude != location.getLatitude() || longitude != location.getLongitude()) {
-                    Log.d("ZooSeeker", String.format("GPS Location changed: %s", location));
-                    changeLocation(location.getLatitude(), location.getLongitude());
-                }
-            }// 32.7337949159672, -117.1769866067953
-        };
-
-
-        var provider = LocationManager.GPS_PROVIDER;
-        try {
-            locationManager.requestLocationUpdates(provider, 0, 0f, locationListener);
-        }
-        catch(SecurityException se){
-            se.printStackTrace();
-        }
->>>>>>>> origin/detect-offroute:app/src/main/java/com/team63/zooseeker/RealLocationSubject.java
     }
 
     private HashSet<LocationObserver> observers = new HashSet<>();
